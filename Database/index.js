ac296485902e9ee2f8072b7198b0e36d2c9191f5
Ishitasurat25 <ishitasurati83@gmail.com>
2024-08-01 23:30:04 +0530

    update

--- Database/index.js
+++ Database/index.js
@@ -1,6 +1,7 @@
 const dbConnect = require("./db")
 const User = require("./user.schema")
 const express=require("express")
+const isValid = require("./validate")
 const app=express()
 app.use(express.json())
 
@@ -9,16 +10,25 @@ app.use(express.json())
 app.get("/",async(req,res)=>{
     let data=await User.find()
     res.send(data)
-    // console.log("hello");
-    // res.send("working")
 })
 
-app.post("/",async(req,res)=>{
-    // console.log(req.body);
-    // res.send("done")
+app.post("/" ,isValid ,async(req,res)=>{
     let data=await User.create(req.body)
     res.send(data)
 })
+
+app.delete("/:id",async(res,req)=>{
+    let{id}=req.params;
+    res.send("delete");
+    let data=await User.findByIdAndDelete(id)
+})
+
+app.patch("/:id",async(req,res)=>{
+    let{id}=req.params
+    let data=await User.findByIdAndUpdate(id,req.body)
+    res.send(data)
+})
+
 app.listen(8090,()=>{
     console.log("Listening on port 8090");
     dbConnect()
